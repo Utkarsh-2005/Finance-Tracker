11d75652c6f43187329b2897264f0b4c7f1b0aeb
Utkarsh <utkarshjha.4009@gmail.com>
2024-10-27 19:08:14 +0530

    .env

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,7 @@
 const mongoos = require('mongoose');
 const connectDB = async()=>{
     try {
-        await mongoos.connect('mongodb+srv://utkarshjha4009:fAOM73aaiHMLqXTr@cluster0.70ben.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoos.connect(process.env.URL)
             console.log("Connected!!!")
     } catch (error) {
         console.log("Not Connected!!")
